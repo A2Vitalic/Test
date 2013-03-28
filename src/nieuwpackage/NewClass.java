3af356e1c6773742782bb58be21f92b7e841bce1
Wout <Wout@WoutHelsenPHL>
2013-03-28 09:26:12 +0100

    m to the essage

---
+++ src/nieuwpackage/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nieuwpackage;
+
+/**
+ *
+ * @author Wout
+ */
+public class NewClass {
+    
+}
